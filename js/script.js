37bb2bb52b9abe7daa1dd6132923b5eedf541f68
Bachu90 <g.bach90@gmail.com>
2019-03-18 21:27:00 +0100

    18.03.2019

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+document.addEventListener('DOMContentLoaded', function () {
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+
+    var parallax = document.querySelectorAll('.parallax');
+    M.Parallax.init(parallax);
+
+    var collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible);
+
+    var modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+
+    var scrollspy = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(scrollspy);
 });
