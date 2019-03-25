50f4034db84d007578508a72e48e2dab222e9b4a
Bachu90 <g.bach90@gmail.com>
2019-03-25 23:05:29 +0100

    25.03.2019

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
+function changeImage(screenWidth) {
+    if (screenWidth > 1367) {
+        document.getElementById("paralax1").src = "../img/fruits_1920.jpg";
+        document.getElementById("paralax2").src = "../img/running_1920.jpg";
+    } else if (screenWidth < 1367 && screenWidth > 800) {
+        document.getElementById("paralax1").src = "../img/fruits_1366.jpg";
+        document.getElementById("paralax2").src = "../img/running_1366.jpg";
+    } else if (screenWidth < 800) {
+        document.getElementById("paralax1").src = "../img/fruits_mobile.jpg";
+        document.getElementById("paralax2").src = "../img/running_mobile.jpg";
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    var sidenav = document.querySelectorAll('.sidenav');
+    let screenWidth = window.outerHeight;
+    changeImage(screenWidth);
+
+    window.addEventListener('resize', function () {
+        screenWidth = window.outerHeight
+        changeImage(screenWidth);
+    })
+
+    /* Materialize Stuff */
+    const sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 
-    var parallax = document.querySelectorAll('.parallax');
+    const parallax = document.querySelectorAll('.parallax');
     M.Parallax.init(parallax);
 
-    var collapsible = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(collapsible);
+    const collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible, { accordion: false });
 
-    var modal = document.querySelectorAll('.modal');
+    const tabs = document.querySelectorAll('.tabs');
+    M.Tabs.init(tabs);
+
+    const modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
 
-    var scrollspy = document.querySelectorAll('.scrollspy');
+    const scrollspy = document.querySelectorAll('.scrollspy');
     M.ScrollSpy.init(scrollspy);
 });
+
+
