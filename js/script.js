761fc70d94f5f884f79562a12a6f90992bd91e00
Bachu90 <g.bach90@gmail.com>
2019-03-29 22:34:43 +0100

    29.03.2019

--- js/script.js
+++ js/script.js
@@ -12,12 +12,62 @@ function changeImage(screenWidth) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    /* Change Images */
+
     let screenWidth = window.outerHeight;
     changeImage(screenWidth);
 
     window.addEventListener('resize', function () {
         screenWidth = window.outerHeight
         changeImage(screenWidth);
+    })
+
+    /* Sending email */
+
+    const form = document.getElementById("contact-form");
+
+
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        const data = {
+            "first_name": form['first_name'].value,
+            "email": form['email'].value,
+            "subject": form['subject'].value,
+            "message": form['message'].value
+        };
+
+        console.log(data);
+
+        form.reset()
+
+        fetch("https://food-and-diet.pl/email", {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        }).then(response => {
+            if (response.ok && response.status == 200) {
+                console.log("Wysłane!")
+            } else {
+                console.log("Błąd!")
+            }
+        }).catch(err => {
+            console.log(err.message)
+        })
+    }
+
+    form.addEventListener('submit', e => {
+        sendEmail(e);
     })
 
     /* Materialize Stuff */
